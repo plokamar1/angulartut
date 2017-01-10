53e840eb964800efc5ff4c8695a8043fe7b06386
plokamar1 <p.dorgiak@gmail.com>
2017-01-10 12:16:06 +0200

    In this one i generate different 'feelings' using two different controllers

--- js/exam2.js
+++ js/exam2.js
@@ -4,3 +4,15 @@ app2.controller('ctrl1',function($scope){
     $scope.randomNum1 = Math.floor((Math.random()*10) + 1);
     $scope.randomNum2 = Math.floor((Math.random()*10) + 1);
 });
+
+app2.controller('badCtrl',function($scope){
+   var badFeelings = ["Disregarded", "Unimportant", "Rejected", "Powerless"];
+
+   $scope.bad = badFeelings[Math.floor(( Math.random() * 4 ))];
+});
+
+app2.controller('goodCtrl',function($scope){
+   var goodFeelings = ["Pleasure", "Awesome", "Lovable", "Inner Peace"];
+
+   $scope.good = goodFeelings[Math.floor( Math.random() * 4 )];
+}); 
